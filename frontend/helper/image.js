50b989afdfbb600c9d194f5bcf7ec763e029e522
Nidhi <nidhibasrani54@gmail.com>
2024-04-18 19:07:48 +0530

    hello

--- frontend/helper/image.js
+++ frontend/helper/image.js
@@ -5,8 +5,9 @@ import slider3 from "../public/slider-3.jpg"
 import people from "../public/people.png"
 import handshake from "../public/handshake.png"
 import satisfaction from "../public/satisfaction.png"
+import reunion from "../public/reunion.mp4"
 
 export default  {
-    logo,slider1,slider2,slider3,people,handshake,satisfaction,
+    logo,slider1,slider2,slider3,people,handshake,satisfaction,reunion
     
 }
