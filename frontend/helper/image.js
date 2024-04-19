c7f03a35dfae554c808e8288564e7100c0484f25
Nidhi <nidhibasrani54@gmail.com>
2024-04-19 14:02:24 +0530

    changes

--- frontend/helper/image.js
+++ frontend/helper/image.js
@@ -6,7 +6,7 @@ import people from "../public/people.png"
 import handshake from "../public/handshake.png"
 import satisfaction from "../public/satisfaction.png"
 import reunion from "../public/reunion.mp4"
-import aboutimage from "../public/aboutUs.webp"
+import aboutimage from "../public/aboutUs.jpg"
 export default  {
     logo,slider1,slider2,slider3,people,handshake,satisfaction,reunion,aboutimage
     
